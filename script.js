896cf90c1bce503034a22918f58b0c82492be772
Evers Rivero <evers.rivero@gmail.com>
2016-09-11 18:03:48 -0500

    calculadora terminada

--- script.js
+++ script.js
@@ -1,17 +1,61 @@
-/* SCRIPT CALC */
+/***************
+ * SCRIPT CALC *
+ ***************/
 $(document).ready(function() {
 	$('#0,#1,#2,#3,#4,#5,#6,#7,#8,#9').click(function(event) {
 		var a = $('#dato').val();
 		var b = $(this).attr('id');
-		if( (a === '' && b === '0') || (a === '0' && b === '0') ) {
-			$('#dato').val('0').focus();
-		} else {
-			$('#dato').val(a + b).focus();
-		}
+		$('#dato').val( a + b).focus();
 	});
+	$('#limpiar,#iniciar,#retroceso,#divide,#multiplica,#resta,#suma,#coma,#igual').click(function(event) {
+		var id = $(this).attr('id');
+		var op = $('#dato').val();
+		switch (id) {
+			case 'divide':
+				op += '/';
+				break;
+			case 'multiplica':
+				op += '*';
+				break;
+			case 'resta':
+				op += '-';
+				break;
+			case 'suma':
+				op += '+';
+				break;
+			case 'coma':
+				console.log(op.indexOf('.'));
+				if (op.indexOf('.') < 0){
+					op += '.';
+				}
+				break;
+			case 'igual':
+				op = evaluar( $('#dato').val() );
+				break;
+			case 'limpiar':
+				op = '0';
+				break;
+			case 'retroceso':
+				if (op !== ''){
+					var sd = parseInt(op.length - 1);
+					op = op.substr(0, sd);
+				} else {
+					op = 0;
+				}
+				break;
+		}
+		$('#dato').val(op).focus();
+	});
+	// Capturamos las teclas presionadas
 	$(document).on("keydown", keycalc);
 });
 
+var total = 0;
+var operador = null;
+
+/**
+ * Definimos las acciones para las teclas permitidas
+ */
 function keycalc(e) {
 	var teclasPermitidas = [8,13,27,96,97,98,99,100,101,102,103,104,105,106,107,109,110,111];
 	if ($.inArray((e.which || e.keyCode), teclasPermitidas) < 0) {
@@ -19,19 +63,17 @@ function keycalc(e) {
 	} else {
 		switch (e.which || e.keyCode) {
 			case 13: // ENTER
+				var valor = evaluar( $('#dato').val() );
+				$('#dato').val(valor);
 				break;
 			case 27: // ESCAPE
 				$('#dato').val(0);
 				break;
-			case 106: // MULTIPLICA
-				break;
-			case 107: // SUMA
-				break;
-			case 109: // RESTA
-				break;
-			case 111: // DIVIDE
-				break;
 		}
 	}
 	console.log(e.which || e.keyCode);
+}
+
+function evaluar(fn) {
+	return new Function('return ' + fn)();
 }
